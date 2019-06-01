Removed (incorrect) softmax from model output, added dropout after activation functions of FC layers

model.py
@@ -5,7 +5,7 @@ import data
 
 
 class BachNet(torch.nn.Module):
-    def __init__(self, batch_size, output_sizes, hidden_size, context_radius):
+    def __init__(self, batch_size, output_sizes, hidden_size, context_radius, dropout=0.5):
         super().__init__()
         self.batch_size = batch_size
         self.context_radius = context_radius
@@ -14,6 +14,8 @@ class BachNet(torch.nn.Module):
         part_length = data.pitch_size + len(data.indices_parts)
         input_vector_length = (5 * context_radius + 1) * part_length + \
                               (2 * context_radius + 1) * len(data.indices_extra)
+
+        self.dropout = torch.nn.Dropout(dropout)
 
         self.fc_bass_1 = torch.nn.Linear(input_vector_length, hidden_size)
         self.fc_bass_2 = torch.nn.Linear(hidden_size, hidden_size)
@@ -31,25 +33,31 @@ class BachNet(torch.nn.Module):
         inputs_bass = torch.cat([v.view(self.batch_size, -1) for v in inputs.values()], dim=1)
 
         outputs_bass = selu(self.fc_bass_1(inputs_bass))
+        outputs_bass = self.dropout(outputs_bass)
         outputs_bass = selu(self.fc_bass_2(outputs_bass))
+        outputs_bass = self.dropout(outputs_bass)
         outputs_bass = self.fc_bass_3(outputs_bass)
 
         prediction_bass = one_hot(torch.max(outputs_bass, dim=1)[1], self.output_sizes).float()
         inputs_alto = torch.cat([inputs_bass, prediction_bass], dim=1)
 
         outputs_alto = selu(self.fc_alto_1(inputs_alto))
+        outputs_alto = self.dropout(outputs_alto)
         outputs_alto = selu(self.fc_alto_2(outputs_alto))
+        outputs_alto = self.dropout(outputs_alto)
         outputs_alto = self.fc_alto_3(outputs_alto)
 
         prediction_alto = one_hot(torch.max(outputs_alto, dim=1)[1], self.output_sizes).float()
         inputs_tenor = torch.cat([inputs_alto, prediction_alto], dim=1)
 
         outputs_tenor = selu(self.fc_tenor_1(inputs_tenor))
+        outputs_tenor = self.dropout(outputs_tenor)
         outputs_tenor = selu(self.fc_tenor_2(outputs_tenor))
+        outputs_tenor = self.dropout(outputs_tenor)
         outputs_tenor = self.fc_tenor_3(outputs_tenor)
 
         return {
-            'alto': softmax(outputs_alto, dim=1),
-            'tenor': softmax(outputs_tenor, dim=1),
-            'bass': softmax(outputs_bass, dim=1)
+            'alto': outputs_alto,
+            'tenor': outputs_tenor,
+            'bass': outputs_bass
         }